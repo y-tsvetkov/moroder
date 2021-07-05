ed9190fc8da2c4cba7a096425b532355259f3a3f
mr-mecha <48952845+mr-mecha@users.noreply.github.com>
2021-07-05 16:35:10 +0300

    Update moroder_cma.py

--- moroder_cma.py
+++ moroder_cma.py
@@ -10,7 +10,7 @@ import multiprocessing
 import functions
 
 # Important: multiprocessing requires the code to be executed in parallel to be in __main__ 
-# this is why it is required to split the code in a main script (this) and a function script (cma_functions.py)
+# this is why it is required to split the code in a main script (this) and a function script (functions.py)
 
 import matplotlib.pyplot as plt
 
@@ -18,28 +18,28 @@ if __name__ == '__main__' :
   
   # starting parallel workers 
   multiprocessing.set_start_method('spawn', True)
-  workers = Pool(funcs.num_workers)
+  workers = Pool(functions.num_workers)
 
   # training phase (Tensorflow graph activation, performing CMA-ES optimisation)
-  with funcs.sess.as_default() :
-    funcs.sess.run(tf.compat.v1.global_variables_initializer())
+  with functions.sess.as_default() :
+    functions.sess.run(tf.compat.v1.global_variables_initializer())
     saver = tf.compat.v1.train.Saver()
-    cma = CMAES(funcs.NPARAMS,
-    sigma_init=funcs.sigma,
+    cma = CMAES(functions.NPARAMS,
+    sigma_init=functions.sigma,
     weight_decay=0,
-   popsize=funcs.NPOPULATION) 
-  cma_history = funcs.solve(cma,workers)
+   popsize=functions.NPOPULATION) 
+  cma_history = functions.solve(cma,workers)
 
   # loading learned gait after training
-  with open(funcs.save_dir, 'rb') as f:
+  with open(functions.save_dir, 'rb') as f:
     bestparams = pickle.load(f)
-  with open(funcs.max_fit_dir, 'rb') as f:
+  with open(functions.max_fit_dir, 'rb') as f:
     history = pickle.load(f)
 
   # testing learned gait
-  funcs.env[0].reset()
+  functions.env[0].reset()
   time.sleep(2)
-  fitness = funcs.fitness_func(bestparams,True)
+  fitness = functions.fitness_func(bestparams,True)
 
   # showing reward/time graph
   plt.plot(history)
