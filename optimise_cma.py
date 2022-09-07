3cfa6675c6cf4caa935b740b42b7f3f04787bcea
Yordan Tsvetkov <48952845+mr-mecha@users.noreply.github.com>
2022-09-07 01:18:36 +0100

    Update and rename moroder_cma.py to optimise_cma.py

--- optimise_cma.py
+++ optimise_cma.py
@@ -7,10 +7,10 @@ import time
 import pickle
 from multiprocessing import Pool
 import multiprocessing
-import functions
+import optim_functions as optim
 
 # Important: multiprocessing requires the code to be executed in parallel to be in __main__ 
-# this is why it is required to split the code in a main script (this) and a function script (functions.py)
+# this is why it is required to split the code in a main script (this) and a function script (training_functions.py)
 
 import matplotlib.pyplot as plt
 
@@ -18,28 +18,28 @@ if __name__ == '__main__' :
   
   # starting parallel workers 
   multiprocessing.set_start_method('spawn', True)
-  workers = Pool(functions.num_workers)
+  workers = Pool(optim.num_workers)
 
   # training phase (Tensorflow graph activation, performing CMA-ES optimisation)
-  with functions.sess.as_default() :
-    functions.sess.run(tf.compat.v1.global_variables_initializer())
-    saver = tf.compat.v1.train.Saver()
-    cma = CMAES(functions.NPARAMS,
-    sigma_init=functions.sigma,
+  with optim.sess.as_default() :
+    optim.sess.run(tf.compat.v1.global_variables_initializer())
+    saver = tf.compat.v1.optim.Saver()
+    cma = CMAES(optim.NPARAMS,
+    sigma_init=optim.sigma,
     weight_decay=0,
-   popsize=functions.NPOPULATION) 
-  cma_history = functions.solve(cma,workers)
+   popsize=optim.NPOPULATION) 
+  cma_history = optim.solve(cma,workers)
 
   # loading learned gait after training
-  with open(functions.save_dir, 'rb') as f:
+  with open(optim.save_dir, 'rb') as f:
     bestparams = pickle.load(f)
-  with open(functions.max_fit_dir, 'rb') as f:
+  with open(optim.max_fit_dir, 'rb') as f:
     history = pickle.load(f)
 
   # testing learned gait
-  functions.env[0].reset()
+  optim.env[0].reset()
   time.sleep(2)
-  fitness = functions.fitness_func(bestparams,True)
+  fitness = optim.fitness_func(bestparams,True)
 
   # showing reward/time graph
   plt.plot(history)
